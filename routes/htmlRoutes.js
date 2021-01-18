d4a2a630059ac6a947b6854a156dc3e579cf43d3
JRob <72467031+Jonathan-84@users.noreply.github.com>
2021-01-18 00:03:31 -0500

    still trying to find my error

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,17 +1,15 @@
-const { Router } = require("express");
+//const { Router } = require("express");
 
 const path = require("path");
 const router= require("express").Router();
 
-
-// Routes user to the index page
-router.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
 // Routes user to the notes page
 router.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+// Routes user to the index page
+router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports= router
